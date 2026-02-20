5f9457c716fe8b073fef5edea4db5597219b4ff4
LucasMTC <lucas.callisperis@gmail.com>
2026-02-19 18:21:39 -0600

    Lab 1

---
+++ src/Tercer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {bancos} from './assets/bancos'
+
+const Tercer = () => {
+    return (
+        <div>
+            <h1>Lista de Bancos</h1>
+            <ul>
+                {bancos.map((b) => (
+                    <li key={b.id}>
+                        {b.id} - {b.name}, {b.country}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Tercer
